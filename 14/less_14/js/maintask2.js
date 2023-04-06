a2bf0c1e5b3cf38f004cd7cfa066eb8d6f00ef68
Puiu Dumitru99 <puiudumitru99@gmail.com>
2023-04-06 16:32:50 +0300

    Done 50% HW14

--- 14/less_14/js/maintask2.js
+++ 14/less_14/js/maintask2.js
@@ -1,19 +1,44 @@
-
-
-
-
+butn=document.querySelector('.btn');
+all_inp=document.querySelectorAll('input')
+ 
+// function verification if intut have someone
 function check()
 {
-input_name=document.querySelector('.name').value;
-input_phone=document.querySelector('.phone').value;
-
-console.log(input_name)
-if(input_name=="" && input_phone!=="")
+inpname=document.querySelector('.name').value;
+inpphone=document.querySelector('.phone').value;
+a=[inpname,inpphone];
+if(inpname=='' && inpphone=='')
 {
-alert("Nu ati introdus numele"); 
+    return alert("Nu ati introdus datele")
+} 
+if(inpname=='' && inpphone!='')
+{
+    return alert("Nu ati introdus numele")
 }
- return alert("Buna ziua"+input_name,input_phone);
-
-alert(input_name);
+if(inpname!='' && inpphone=='')
+{
+    return alert("Nu ati introdus Numarul de telefon")
+}
+else alert("Datele au fost trimise") 
 
 }
+butn.addEventListener('click',check);
+// finish
+
+all_inp.forEach(function(item,i)
+   { // color input if click on input
+    item.addEventListener('mouseup',function()
+    {
+        this.style.background="#ffb4b4";  
+    });
+});
+    all_inp.forEach(function(item,i)
+   {
+    // funtion for reset color for input
+    item.addEventListener('blur',function()
+    {
+        this.style.background="#eaeaea";
+    });});
+   
+    console.log(all_inp.type);
+
